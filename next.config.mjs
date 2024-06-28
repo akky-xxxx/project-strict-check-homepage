72db3e2abbf0cdbd3733f3c08690fdd61a49c267
akky-xxxx <114.s.aki@gmail.com>
2024-06-28 22:54:41 +0900

    chore: resolve eslint errors

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,9 @@
-import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev"
 
 // Here we use the @cloudflare/next-on-pages next-dev module to allow us to use bindings during local development
 // (when running the application with `next dev`), for more information see:
 // https://github.com/cloudflare/next-on-pages/blob/5712c57ea7/internal-packages/next-dev/README.md
-if (process.env.NODE_ENV === 'development') {
-  await setupDevPlatform();
-}
+if (process.env.NODE_ENV === "development") await setupDevPlatform()
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -19,14 +17,16 @@ const nextConfig = {
   webpack: (config, { dev, isServer }) => {
     if (!dev && !isServer) {
       Object.assign(config.resolve.alias, {
-        "react/jsx-runtime.js": "preact/compat/jsx-runtime",
-        react: "preact/compat",
-        "react-dom/test-utils": "preact/test-utils",
+        // TODO: eslint-config-sc-js@0.0.7 をリリースしたら不要になるので消す
+        // eslint-disable-next-line quote-props
+        "react": "preact/compat",
         "react-dom": "preact/compat",
-      });
+        "react-dom/test-utils": "preact/test-utils",
+        "react/jsx-runtime.js": "preact/compat/jsx-runtime",
+      })
     }
-    return config;
+    return config
   },
-};
+}
 
-export default nextConfig;
+export default nextConfig
