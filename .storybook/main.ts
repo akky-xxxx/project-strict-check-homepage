8364431154b9793b85f33536e0ebce8f447730a7
akky-xxxx <114.s.aki@gmail.com>
2024-07-04 23:14:11 +0900

    chore: remove @storybook/addon-coverage

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -9,7 +9,6 @@ const config: StorybookConfig = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@chromatic-com/storybook",
-    "@storybook/addon-coverage",
     "@storybook/addon-interactions",
   ],
   framework: {
