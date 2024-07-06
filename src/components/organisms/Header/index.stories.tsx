942263db91a1544bc3d6d0454b66df782e9c70a9
akky-xxxx <114.s.aki@gmail.com>
2024-07-07 01:15:04 +0900

    feat: implement Header

--- src/components/organisms/Header/index.stories.tsx
+++ src/components/organisms/Header/index.stories.tsx
@@ -1,28 +1,99 @@
 import { Header } from "."
+import { STORY_PARAMETERS } from "../../../shared/constants/STORY_PARAMETERS"
 
 import type { Meta, StoryObj } from "@storybook/react"
 
 const meta: Meta<typeof Header> = {
   component: Header,
+
+  args: {
+    hasPackageSelector: true,
+  },
 }
 
 export default meta
 type Story = StoryObj<typeof Header>
 
-export const Light: Story = {
-
+export const LightPcHasPackageSelector: Story = {
   parameters: {
-    backgrounds: {
-      default: "light",
-    },
+    ...STORY_PARAMETERS.THEME.LIGHT,
+    ...STORY_PARAMETERS.VIEWPORTS.PC,
   },
 }
 
-export const Dark: Story = {
-
+export const LightTbHasPackageSelector: Story = {
   parameters: {
-    backgrounds: {
-      default: "dark",
-    },
+    ...STORY_PARAMETERS.THEME.LIGHT,
+    ...STORY_PARAMETERS.VIEWPORTS.TB,
+  },
+}
+
+export const LightSpHasPackageSelector: Story = {
+  parameters: {
+    ...STORY_PARAMETERS.THEME.LIGHT,
+    ...STORY_PARAMETERS.VIEWPORTS.SP,
+  },
+}
+
+export const DarkPcHasPackageSelector: Story = {
+  parameters: {
+    ...STORY_PARAMETERS.THEME.DARK,
+    ...STORY_PARAMETERS.VIEWPORTS.PC,
+  },
+}
+
+export const DarkTbHasPackageSelector: Story = {
+  parameters: {
+    ...STORY_PARAMETERS.THEME.DARK,
+    ...STORY_PARAMETERS.VIEWPORTS.TB,
+  },
+}
+
+export const DarkSpHasPackageSelector: Story = {
+  parameters: {
+    ...STORY_PARAMETERS.THEME.DARK,
+    ...STORY_PARAMETERS.VIEWPORTS.SP,
+  },
+}
+
+export const LightPcNotHasPackageSelector: Story = {
+  ...LightPcHasPackageSelector,
+  args: {
+    hasPackageSelector: false,
+  },
+}
+
+export const LightTbNotHasPackageSelector: Story = {
+  ...LightTbHasPackageSelector,
+  args: {
+    hasPackageSelector: false,
+  },
+}
+
+export const LightSpNotHasPackageSelector: Story = {
+  ...LightSpHasPackageSelector,
+  args: {
+    hasPackageSelector: false,
+  },
+}
+
+export const DarkPcNotHasPackageSelector: Story = {
+  ...DarkPcHasPackageSelector,
+  args: {
+    hasPackageSelector: false,
+  },
+}
+
+export const DarkTbNotHasPackageSelector: Story = {
+  ...DarkTbHasPackageSelector,
+  args: {
+    hasPackageSelector: false,
+  },
+}
+
+export const DarkSpNotHasPackageSelector: Story = {
+  ...DarkSpHasPackageSelector,
+  args: {
+    hasPackageSelector: false,
   },
 }
--- src/components/organisms/Header/index.tsx
+++ src/components/organisms/Header/index.tsx
@@ -1,5 +1,87 @@
+"use client"
+
+import { Fragment, useState } from "react"
+
+import { css } from "@panda/css"
+
+import { Drawer } from "./components/atoms/Drawer"
+import { DrawerIcon } from "./components/atoms/DrawerIcon"
+import { MenuItems } from "./components/atoms/MenuItems"
+import { SiteName } from "./components/atoms/SiteName"
+import { ScrollLock } from "../../atoms/ScrollLock"
+
+import type { Theme } from "../../../shared/types/Theme"
 import type { FC } from "react"
 
-export const Header: FC = () => {
-  return <div>Header</div>
+type Props = {
+  hasPackageSelector?: boolean
+  themeFromServerCookie: Theme
 }
+
+export const Header: FC<Props> = (props) => {
+  const {
+    hasPackageSelector,
+    themeFromServerCookie,
+  } = props
+
+  const [isOpenedMenu, setIsOpenedMenu] = useState(false)
+  const handleToggleMenu = () => {
+    setIsOpenedMenu((current) => !current)
+  }
+
+  return (
+    <Fragment>
+      <ScrollLock isLock={isOpenedMenu} />
+      <header className={root}>
+        <SiteName />
+
+        <div className={drawerIconWrapper}>
+          <DrawerIcon handleClick={handleToggleMenu} isOpened={isOpenedMenu} />
+        </div>
+        <div className={rightItems}>
+          <MenuItems
+            hasPackageSelector={hasPackageSelector}
+            themeFromServerCookie={themeFromServerCookie}
+          />
+        </div>
+      </header>
+      <Drawer isOpened={isOpenedMenu}>
+        <MenuItems
+          hasPackageSelector={hasPackageSelector}
+          themeFromServerCookie={themeFromServerCookie}
+        />
+      </Drawer>
+    </Fragment>
+  )
+}
+
+const root = css({
+  alignItems: "center",
+  borderBottomColor: "backgroundColor.case2",
+  borderBottomStyle: "solid",
+  borderBottomWidth: 2,
+  display: "flex",
+  height: "var(--header-height)",
+  paddingInline: 12,
+})
+
+const drawerIconWrapper = css({
+  display: {
+    base: "block",
+    md: "none",
+  },
+  marginLeft: {
+    base: "auto",
+    md: "initial",
+  },
+})
+
+const rightItems = css({
+  display: {
+    base: "none",
+    md: "initial",
+  },
+  marginLeft: {
+    md: "auto",
+  },
+})
