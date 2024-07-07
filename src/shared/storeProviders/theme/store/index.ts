1a731525a1da159385bc0f670f0624fb02a5b1cf
akky-xxxx <114.s.aki@gmail.com>
2024-07-07 20:01:20 +0900

    feat: introduce cookieStorage into themeStore

--- src/shared/storeProviders/theme/store/index.ts
+++ src/shared/storeProviders/theme/store/index.ts
@@ -1,8 +1,20 @@
+import Cookies from "js-cookie"
+import { persist, createJSONStorage, devtools, type StateStorage } from "zustand/middleware"
 import { createStore } from "zustand/vanilla"
+
+import { COOKIE_KEY_THEME } from "@shared/constants/COOKIE_KEYS"
 
 import type { ThemeState } from "./types/ThemeState"
 import type { ThemeStore } from "./types/ThemeStore"
 import type { Theme } from "@shared/types/Theme"
+
+const cookieStorage: StateStorage = {
+  getItem: () => null,
+  removeItem: () => null,
+  setItem: (name: string, value: string) => {
+    Cookies.set(name, value)
+  },
+}
 
 const initializeStore = (theme: Theme): ThemeState => ({ theme })
 
@@ -12,12 +24,22 @@ const defaultInitState: ThemeState = {
 
 const createThemeStore = (
   initState: ThemeState = defaultInitState,
-) => createStore<ThemeStore>()((set) => ({
-  ...initState,
-  setTheme: (newTheme: Theme) => {
-    set({ theme: newTheme })
-  },
-}))
+) => createStore<ThemeStore>()(
+  devtools(
+    persist(
+      (set) => ({
+        ...initState,
+        setTheme: (newTheme: Theme) => {
+          set({ theme: newTheme })
+        },
+      }),
+      {
+        name: COOKIE_KEY_THEME,
+        storage: createJSONStorage(() => cookieStorage),
+      },
+    ),
+  ),
+)
 
 export const store = {
   createStore: createThemeStore,
