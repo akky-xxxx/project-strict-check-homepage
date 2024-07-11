9f6d46b65c3a6a6858f794421d1b76726de1e336
akky-xxxx <114.s.aki@gmail.com>
2024-07-11 22:40:34 +0900

    test: add tests for GithubLink

--- src/components/organisms/Header/components/atoms/MenuItems/components/atoms/GithubLink/index.stories.tsx
+++ src/components/organisms/Header/components/atoms/MenuItems/components/atoms/GithubLink/index.stories.tsx
@@ -1,3 +1,6 @@
+import { expect, within } from "@storybook/test"
+
+
 import { STORY_PARAMETERS } from "@shared/constants/STORY_PARAMETERS"
 
 import { GithubLink } from "./index"
@@ -44,5 +47,20 @@ export const DarkSp: Story = {
   parameters: {
     ...STORY_PARAMETERS.THEME.DARK,
     ...STORY_PARAMETERS.VIEWPORTS.SP,
+  },
+}
+
+export const LinkAttributesTest: Story = {
+  tags: ["!autodocs"],
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement)
+
+    const LinkElement = canvas.getByLabelText("Github")
+
+    await expect(LinkElement).toBeInTheDocument()
+    await expect(LinkElement.nodeName).toBe("A")
+    await expect(LinkElement).toHaveAttribute("href", "https://github.com/akky-xxxx/strict-check")
+    await expect(LinkElement).toHaveAttribute("target", "_blank")
+    await expect(LinkElement).toHaveAttribute("rel", "noopener noreferrer")
   },
 }
