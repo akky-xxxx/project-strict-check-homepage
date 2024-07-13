95d16825c1ed5308227d837e49b3da20505b7c8b
akky-xxxx <114.s.aki@gmail.com>
2024-07-14 00:34:51 +0900

    feat: update TOKEN_FONT_SIZES

--- src/config/panda/THEME/constants/TOKEN_FONT_SIZES/index.ts
+++ src/config/panda/THEME/constants/TOKEN_FONT_SIZES/index.ts
@@ -6,4 +6,7 @@ export const TOKEN_FONT_SIZES = {
   SIZE_18: { value: "calc(18 / var(--root-font-size) * 1rem)" },
   SIZE_20: { value: "calc(20 / var(--root-font-size) * 1rem)" },
   SIZE_22: { value: "calc(22 / var(--root-font-size) * 1rem)" },
+  SIZE_24: { value: "calc(24 / var(--root-font-size) * 1rem)" },
+  SIZE_28: { value: "calc(28 / var(--root-font-size) * 1rem)" },
+  SIZE_32: { value: "calc(32 / var(--root-font-size) * 1rem)" },
 } as const satisfies Tokens["fontSizes"]
