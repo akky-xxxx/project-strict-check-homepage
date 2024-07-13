39e37347a434f3719832f3cdec44ccf22d45d3dd
akky-xxxx <114.s.aki@gmail.com>
2024-07-14 00:40:36 +0900

    feat: update SEMANTIC_TOKEN_FONT_SIZES

--- src/config/panda/THEME/constants/SEMANTIC_TOKEN_FONT_SIZES/index.ts
+++ src/config/panda/THEME/constants/SEMANTIC_TOKEN_FONT_SIZES/index.ts
@@ -3,14 +3,24 @@ import type { SemanticTokens } from "@pandacss/dev"
 export const SEMANTIC_TOKEN_FONT_SIZES = {
   fontSize: {
     DEFAULT: { value: "{fontSizes.SIZE_16}" },
-
-    lg: { value: "{fontSizes.SIZE_20}" },
     sm: { value: "{fontSizes.SIZE_14}" },
-    xl: { value: "{fontSizes.SIZE_22}" },
   },
 
   siteName: {
+    DEFAULT: { value: "{fontSizes.SIZE_20}" },
+    md: { value: "{fontSizes.SIZE_22}" },
+  },
+
+  heading1: {
+    DEFAULT: { value: "{fontSizes.SIZE_28}" },
+    md: { value: "{fontSizes.SIZE_32}" },
+  },
+  heading2: {
     DEFAULT: { value: "{fontSizes.SIZE_22}" },
+    md: { value: "{fontSizes.SIZE_24}" },
+  },
+  heading3: {
+    DEFAULT: { value: "{fontSizes.SIZE_18}" },
     md: { value: "{fontSizes.SIZE_20}" },
   },
 } as const satisfies SemanticTokens["fontSizes"]
