146688ea6ef1733db2bb069a09ff4a5c80e97c7a
akky-xxxx <114.s.aki@gmail.com>
2024-07-14 20:38:41 +0900

    feat: use PackageManagerProvider at MainContentProviders

--- src/components/organisms/MainContentProviders/index.tsx
+++ src/components/organisms/MainContentProviders/index.tsx
@@ -1,16 +1,20 @@
 import { EslintConfigProvider } from "@shared/storeProviders/eslintConfig"
+import { PackageManagerProvider } from "@shared/storeProviders/packageManager"
 
 import type { FCWithChildren } from "@shared/types/FCWithChildren"
 import type { ComponentProps } from "react"
 
-type Props = ComponentProps<typeof EslintConfigProvider>
+type Props = ComponentProps<typeof EslintConfigProvider> &
+  ComponentProps<typeof PackageManagerProvider>
 
 export const MainContentProviders: FCWithChildren<Props> = (props) => {
-  const { children, eslintConfig } = props
+  const { children, eslintConfig, packageManager } = props
 
   return (
-    <EslintConfigProvider eslintConfig={eslintConfig}>
-      {children}
-    </EslintConfigProvider>
+    <PackageManagerProvider packageManager={packageManager}>
+      <EslintConfigProvider eslintConfig={eslintConfig}>
+        {children}
+      </EslintConfigProvider>
+    </PackageManagerProvider>
   )
 }
