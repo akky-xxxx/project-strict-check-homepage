dfaa286706a5fc1c8338a12ddbd9a26251300dea
akky-xxxx <114.s.aki@gmail.com>
2024-07-14 20:30:07 +0900

    feat: define COOKIE_KEY_PACKAGE_MANAGER

--- src/shared/constants/COOKIE_KEYS/index.ts
+++ src/shared/constants/COOKIE_KEYS/index.ts
@@ -4,4 +4,5 @@ type CookieKey = keyof typeof nextCookiesSchemas
 
 /* eslint-disable sc-js/forbidden-multiple-named-exports */
 export const COOKIE_KEY_ESLINT_CONFIG = "eslintConfig" satisfies CookieKey
+export const COOKIE_KEY_PACKAGE_MANAGER = "packageManager" satisfies CookieKey
 export const COOKIE_KEY_THEME = "theme" satisfies CookieKey
