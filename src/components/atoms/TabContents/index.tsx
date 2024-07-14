c9f5c12d4324a0ddc4afe1f158203ed851b65b43
akky-xxxx <114.s.aki@gmail.com>
2024-07-14 18:02:28 +0900

    feat: implement TabContents

---
+++ src/components/atoms/TabContents/index.tsx
@@ -0,0 +1,61 @@
+"use client"
+
+import { useId } from "react"
+
+import { cx } from "@panda/css"
+
+import { styles } from "./styles"
+
+import type { TabContent } from "./types/TabContent"
+import type { ReactNode } from "react"
+
+type Props<L extends string = string> = {
+  contents: Array<TabContent<L>>
+  selectedLabel: L
+  handleSelectLabel: (label: L) => void
+}
+
+const { activeTabItem, contentStyle, hiddenRadio, tabItem, tabs } = styles
+
+export const TabContents = <L extends string = string>(props: Props<L>): ReactNode => {
+  const { contents, selectedLabel, handleSelectLabel } = props
+  const id = useId()
+
+  return (
+    <div>
+      <ul className={tabs}>
+        {contents.map((content) => {
+          const { label } = content
+          const onClickLabel = () => {
+            handleSelectLabel(label)
+          }
+          const tabItemStyle = [
+            tabItem,
+            label === selectedLabel && activeTabItem,
+          ]
+          return (
+            <li key={label}>
+              <label htmlFor={`${label}-${id}`}>
+                <button className={cx(...tabItemStyle)} type="button" onClick={onClickLabel}>
+                  {label}
+                </button>
+              </label>
+            </li>
+          )
+        })}
+      </ul>
+      <div>
+        {contents.map((content) => {
+          const { label, contentInner } = content
+
+          return (
+            <div className={contentStyle}>
+              {contentInner}
+              <input checked={label === selectedLabel} className={hiddenRadio} id={`${label}-${id}`} name="tab" type="radio" />
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
---
+++ src/components/atoms/TabContents/styles/index.ts
@@ -0,0 +1,53 @@
+import { css } from "@panda/css"
+
+const BACKGROUND_COLOR_CASE2 = "backgroundColor.case2"
+
+const tabs = css({
+  background: BACKGROUND_COLOR_CASE2,
+  borderBottomColor: "backgroundColor.case3",
+  borderBottomStyle: "solid",
+  borderBottomWidth: 1,
+  borderTopLeftRadius: { md: 8 },
+  borderTopRightRadius: { md: 8 },
+  display: "flex",
+  flexWrap: "nowrap",
+  overflowX: "auto",
+  paddingInline: { md: 12 },
+})
+
+const tabItem = css({
+  backgroundColor: BACKGROUND_COLOR_CASE2,
+  borderBottomColor: BACKGROUND_COLOR_CASE2,
+  borderBottomStyle: "solid",
+  borderBottomWidth: 1,
+  cursor: "pointer",
+  padding: 12,
+  transition: "border-bottom-color 0.2s ease-in-out, background-color 0.2s ease-in-out",
+})
+
+const activeTabItem = css({
+  backgroundColor: "color.highlight",
+  borderBottomColor: "color.accent",
+  borderBottomStyle: "solid",
+  borderBottomWidth: 1,
+})
+
+const contentStyle = css({
+  display: "none",
+
+  _hasChecked: {
+    display: "block",
+  },
+})
+
+const hiddenRadio = css({
+  display: "none",
+})
+
+export const styles = {
+  activeTabItem,
+  contentStyle,
+  hiddenRadio,
+  tabItem,
+  tabs,
+} as const
