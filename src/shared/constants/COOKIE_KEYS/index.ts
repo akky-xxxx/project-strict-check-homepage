a2ac676677368a728a4cd4dc741c57cce2ad3b07
akky-xxxx <114.s.aki@gmail.com>
2024-07-14 20:17:42 +0900

    feat: define COOKIE_KEY_ESLINT_CONFIG

--- src/shared/constants/COOKIE_KEYS/index.ts
+++ src/shared/constants/COOKIE_KEYS/index.ts
@@ -2,4 +2,6 @@ import type { nextCookiesSchemas } from "../../schemas/nextCookiesSchemas"
 
 type CookieKey = keyof typeof nextCookiesSchemas
 
+/* eslint-disable sc-js/forbidden-multiple-named-exports */
+export const COOKIE_KEY_ESLINT_CONFIG = "eslintConfig" satisfies CookieKey
 export const COOKIE_KEY_THEME = "theme" satisfies CookieKey
