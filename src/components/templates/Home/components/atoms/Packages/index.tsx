c63f174ebd7973e39b143f3ec5c5795bc5e1bfee
akky-xxxx <114.s.aki@gmail.com>
2024-07-15 13:24:03 +0900

    refactor: change to managed by array

--- src/components/templates/Home/components/atoms/Packages/index.tsx
+++ src/components/templates/Home/components/atoms/Packages/index.tsx
@@ -4,52 +4,56 @@ import { Card } from "../../../../../atoms/Card"
 import { Heading2 } from "../../../../../atoms/Heading2"
 import { Section } from "../../../../../atoms/Section"
 
-import type { FC } from "react"
+import type { ComponentProps, FC } from "react"
+
+const CARD_DATA = [
+  {
+    description: "All in one package that eslint config sc series.",
+    href: "/eslint-config-all",
+    title: "eslint-config-all",
+  },
+  {
+    description: "eslint config for javascript.",
+    href: "/eslint-config-js",
+    title: "eslint-config-js",
+  },
+  {
+    description: "eslint config for typescript.",
+    href: "/eslint-config-ts",
+    title: "eslint-config-ts",
+  },
+  {
+    description: "eslint config for Jest.",
+    href: "/eslint-config-jest",
+    title: "eslint-config-jest",
+  },
+  {
+    description: "eslint config for Storybook.",
+    href: "/eslint-config-storybook",
+    title: "eslint-config-storybook",
+  },
+  {
+    description: "eslint config for Next.js.",
+    href: "/eslint-config-next",
+    title: "eslint-config-next",
+  },
+  {
+    description: "eslint config for React.js.",
+    href: "/eslint-config-react",
+    title: "eslint-config-react",
+  },
+  {
+    description: "eslint plugin for javascript.",
+    href: "/eslint-plugin-js",
+    title: "eslint-plugin-js",
+  },
+] as const satisfies Array<ComponentProps<typeof Card>>
 
 export const Packages: FC = () => (
   <Section>
     <Heading2>Packages</Heading2>
     <div className={packagesCardWrapper}>
-      <Card
-        description="All in one package that eslint config sc series."
-        href="/eslint-config-all"
-        title="eslint-config-all"
-      />
-      <Card
-        description="eslint config for javascript."
-        href="/eslint-config-js"
-        title="eslint-config-js"
-      />
-      <Card
-        description="eslint config for typescript."
-        href="/eslint-config-ts"
-        title="eslint-config-ts"
-      />
-      <Card
-        description="eslint config for Jest."
-        href="/eslint-config-jest"
-        title="eslint-config-jest"
-      />
-      <Card
-        description="eslint config for Storybook."
-        href="/eslint-config-storybook"
-        title="eslint-config-storybook"
-      />
-      <Card
-        description="eslint config for Next.js."
-        href="/eslint-config-next"
-        title="eslint-config-next"
-      />
-      <Card
-        description="eslint config for React.js."
-        href="/eslint-config-react"
-        title="eslint-config-react"
-      />
-      <Card
-        description="eslint plugin for javascript."
-        href="/eslint-plugin-js"
-        title="eslint-plugin-js"
-      />
+      {CARD_DATA.map((card) => <Card key={card.title} {...card} />)}
     </div>
   </Section>
 )
