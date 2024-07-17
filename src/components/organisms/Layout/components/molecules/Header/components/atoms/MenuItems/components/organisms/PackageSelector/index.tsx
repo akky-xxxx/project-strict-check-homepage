f5a1e51cc40f6f0dd889c6de8f4f9a632119ccb4
akky-xxxx <114.s.aki@gmail.com>
2024-07-17 23:00:49 +0900

    feat: separate two views by responsive

--- src/components/organisms/Layout/components/molecules/Header/components/atoms/MenuItems/components/organisms/PackageSelector/index.tsx
+++ src/components/organisms/Layout/components/molecules/Header/components/atoms/MenuItems/components/organisms/PackageSelector/index.tsx
@@ -8,58 +8,83 @@ export const PackageSelector: FC = () => {
   const {
     categoryItems,
     handleChangeCategoryName,
+    handleChangePackageName,
     handleChangeTargetName,
     packageName,
+    packageNameItems,
     targetItems,
   } = usePackageSelector()
   return (
-    <div className={cx(root, "PackageSelector")}>
-      <select disabled className={cx(select, linter)} value="eslint">
-        <option>eslint</option>
-      </select>
+    <div className="PackageSelector">
+      <div className={overTb}>
+        <select disabled className={cx(select, linter)} value="eslint">
+          <option>eslint</option>
+        </select>
 
-      <span>-</span>
+        <span>-</span>
 
-      <select
-        className={cx(select, category)}
-        value={packageName.category}
-        onChange={handleChangeCategoryName}
-      >
-        {categoryItems.map((categoryItem) => (
-          <option key={categoryItem} value={categoryItem}>
-            {categoryItem}
-          </option>
-        ))}
-      </select>
+        <select
+          className={cx(select, category)}
+          value={packageName.category}
+          onChange={handleChangeCategoryName}
+        >
+          {categoryItems.map((categoryItem) => (
+            <option key={categoryItem} value={categoryItem}>
+              {categoryItem}
+            </option>
+          ))}
+        </select>
 
-      <span>-</span>
+        <span>-</span>
 
-      <select
-        className={cx(select, target)}
-        value={packageName.target}
-        onChange={handleChangeTargetName}
-      >
-        {targetItems.map((targetItem) => (
-          <option key={targetItem} value={targetItem}>
-            {targetItem}
-          </option>
-        ))}
-      </select>
+        <select
+          className={cx(select, target)}
+          value={packageName.target}
+          onChange={handleChangeTargetName}
+        >
+          {targetItems.map((targetItem) => (
+            <option key={targetItem} value={targetItem}>
+              {targetItem}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <div className={onlySp}>
+        <select
+          className={cx(select, packageNameStyle)}
+          value={`${packageName.linter}-${packageName.category}-${packageName.target}`}
+          onChange={handleChangePackageName}
+        >
+          {packageNameItems.map((record) => (
+            <option key={record} value={record}>
+              {record}
+            </option>
+          ))}
+        </select>
+      </div>
     </div>
   )
 }
 
-const root = css({
+const overTb = css({
   alignItems: "center",
-  columnGap: {
-    base: 4,
-    lg: 8,
+  columnGap: 8,
+  display: {
+    base: "none",
+    md: "flex",
   },
-  display: "flex",
-  height: {
-    base: 48,
-    md: 30,
+  height: 30,
+})
+
+const onlySp = css({
+  alignItems: "center",
+  columnGap: 4,
+  display: {
+    base: "flex",
+    md: "none",
   },
+  height: 48,
 })
 
 const select = css({
@@ -85,4 +110,8 @@ const category = css({
 
 const target = css({
   width: 112,
+})
+
+const packageNameStyle = css({
+  width: 220,
 })
