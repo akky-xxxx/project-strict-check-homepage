c9eaae9f7d0a1237badbbb75bc03aab5d2fd1064
akky-xxxx <114.s.aki@gmail.com>
2024-07-17 22:57:43 +0900

    feat: return props related packageName from usePackageSelector

--- src/components/organisms/Layout/components/molecules/Header/components/atoms/MenuItems/components/organisms/PackageSelector/modules/usePackageSelector/index.ts
+++ src/components/organisms/Layout/components/molecules/Header/components/atoms/MenuItems/components/organisms/PackageSelector/modules/usePackageSelector/index.ts
@@ -7,17 +7,20 @@ import { packageNameSchema } from "@shared/schemas/packageNameSchema"
 import { usePackageNameStore } from "@shared/storeProviders/packageName"
 
 import { getCategoryItems } from "./modules/getCategoryItems"
+import { getPackageNameItems } from "./modules/getPackageNameItems"
 import { getTargetItems } from "./modules/getTargetItems"
 
 import type { PackageName } from "@shared/types/PackageName"
 import type { ChangeEventHandler } from "react"
 
 const categoryItems = getCategoryItems()
+const packageNameItems = getPackageNameItems()
 
 export const usePackageSelector = () => {
   const {
     packageName,
     setPackageCategoryName,
+    setPackageName,
     setPackageTargetName,
   } = usePackageNameStore((store) => store)
   const [targetItems, setTargetItems] = useState<Array<PackageName["target"]>>(getTargetItems(packageName.category))
@@ -58,11 +61,32 @@ export const usePackageSelector = () => {
     router.push(`/eslint-${newData.category}-${newData.target}`)
   }
 
+  const handleChangePackageName: ChangeEventHandler<HTMLSelectElement> = (event) => {
+    const {
+      currentTarget: { value },
+    } = event
+    const [maybeLinterName, maybeCategoryName, mabyTargetName] = value.split("-")
+    const parseTarget = {
+      category: maybeCategoryName,
+      linter: maybeLinterName,
+      target: mabyTargetName,
+    }
+    const result = safeParse(packageNameSchema, parseTarget)
+    const newData = result.success
+      ? result.output
+      : DEFAULT_PACKAGE_NAME
+    setPackageName(newData)
+    if (newData.target === DEFAULT_PACKAGE_NAME.target) return
+    router.push(`/${value}`)
+  }
+
   return {
     categoryItems,
     handleChangeCategoryName,
+    handleChangePackageName,
     handleChangeTargetName,
     packageName,
+    packageNameItems,
     targetItems,
   }
 }
