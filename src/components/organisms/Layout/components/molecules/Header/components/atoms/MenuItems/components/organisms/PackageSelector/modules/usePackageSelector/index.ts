36d964b8f8ebe700f62a86bb6d4550830d523bae
akky-xxxx <114.s.aki@gmail.com>
2024-07-17 23:26:01 +0900

    refactor: cut out handler definition to new file

--- src/components/organisms/Layout/components/molecules/Header/components/atoms/MenuItems/components/organisms/PackageSelector/modules/usePackageSelector/index.ts
+++ src/components/organisms/Layout/components/molecules/Header/components/atoms/MenuItems/components/organisms/PackageSelector/modules/usePackageSelector/index.ts
@@ -1,84 +1,29 @@
-import { useRouter } from "next/navigation"
 import { useState, useEffect } from "react"
-import { safeParse } from "valibot"
 
-import { DEFAULT_PACKAGE_NAME } from "@shared/constants/DEFAULT_PACKAGE_NAME"
-import { packageNameSchema } from "@shared/schemas/packageNameSchema"
 import { usePackageNameStore } from "@shared/storeProviders/packageName"
 
 import { getCategoryItems } from "./modules/getCategoryItems"
 import { getPackageNameItems } from "./modules/getPackageNameItems"
 import { getTargetItems } from "./modules/getTargetItems"
+import { useGetHandlers } from "./modules/useGetHandlers"
 
 import type { PackageName } from "@shared/types/PackageName"
-import type { ChangeEventHandler } from "react"
 
 const categoryItems = getCategoryItems()
 const packageNameItems = getPackageNameItems()
 
 export const usePackageSelector = () => {
-  const {
-    packageName,
-    setPackageCategoryName,
-    setPackageName,
-    setPackageTargetName,
-  } = usePackageNameStore((store) => store)
+  const { packageName } = usePackageNameStore((store) => store)
   const [targetItems, setTargetItems] = useState<Array<PackageName["target"]>>(getTargetItems(packageName.category))
-  const router = useRouter()
+  const {
+    handleChangeCategoryName,
+    handleChangePackageName,
+    handleChangeTargetName,
+  } = useGetHandlers({ setTargetItems })
+
   useEffect(() => {
     setTargetItems(getTargetItems(packageName.category))
   }, [packageName.category])
-
-  const handleChangeCategoryName: ChangeEventHandler<HTMLSelectElement> = (event) => {
-    const {
-      currentTarget: { value },
-    } = event
-    const parseTarget = {
-      ...packageName,
-      category: value,
-      target: DEFAULT_PACKAGE_NAME.target,
-    }
-    const result = safeParse(packageNameSchema, parseTarget)
-    const nextCategoryName = result.success ? result.output.category : DEFAULT_PACKAGE_NAME.category
-    setPackageCategoryName(nextCategoryName)
-    setTargetItems(getTargetItems(nextCategoryName))
-  }
-
-  const handleChangeTargetName: ChangeEventHandler<HTMLSelectElement> = (event) => {
-    const {
-      currentTarget: { value },
-    } = event
-    const parseTarget = {
-      ...packageName,
-      target: value,
-    }
-    const result = safeParse(packageNameSchema, parseTarget)
-    const newData = result.success
-      ? result.output
-      : DEFAULT_PACKAGE_NAME
-    setPackageTargetName(newData.target)
-    if (newData.target === DEFAULT_PACKAGE_NAME.target) return
-    router.push(`/eslint-${newData.category}-${newData.target}`)
-  }
-
-  const handleChangePackageName: ChangeEventHandler<HTMLSelectElement> = (event) => {
-    const {
-      currentTarget: { value },
-    } = event
-    const [maybeLinterName, maybeCategoryName, maybeTargetName] = value.split("-")
-    const parseTarget = {
-      category: maybeCategoryName,
-      linter: maybeLinterName,
-      target: maybeTargetName,
-    }
-    const result = safeParse(packageNameSchema, parseTarget)
-    const newData = result.success
-      ? result.output
-      : DEFAULT_PACKAGE_NAME
-    setPackageName(newData)
-    if (newData.target === DEFAULT_PACKAGE_NAME.target) return
-    router.push(`/${value}`)
-  }
 
   return {
     categoryItems,
---
+++ src/components/organisms/Layout/components/molecules/Header/components/atoms/MenuItems/components/organisms/PackageSelector/modules/usePackageSelector/modules/useGetHandlers/index.ts
@@ -0,0 +1,63 @@
+import { useRouter } from "next/navigation"
+import { safeParse } from "valibot"
+
+import { DEFAULT_PACKAGE_NAME } from "@shared/constants/DEFAULT_PACKAGE_NAME"
+import { packageNameSchema } from "@shared/schemas/packageNameSchema"
+import { usePackageNameStore } from "@shared/storeProviders/packageName"
+
+import { getTargetItems } from "../getTargetItems"
+
+import type { PackageName } from "@shared/types/PackageName"
+import type { ChangeEventHandler } from "react"
+
+type Input = {
+  setTargetItems: (input: Array<PackageName["target"]>) => void
+}
+
+export const useGetHandlers = (input: Input) => {
+  const { setTargetItems } = input
+  const {
+    packageName,
+    setPackageCategoryName,
+    setPackageName,
+    setPackageTargetName,
+  } = usePackageNameStore((store) => store)
+  const router = useRouter()
+  const handleChangeCategoryName: ChangeEventHandler<HTMLSelectElement> = (event) => {
+    const { currentTarget: { value } } = event
+    const parseTarget = {
+      ...packageName,
+      category: value,
+      target: DEFAULT_PACKAGE_NAME.target,
+    }
+    const result = safeParse(packageNameSchema, parseTarget)
+    const nextCategoryName = result.success ? result.output.category : DEFAULT_PACKAGE_NAME.category
+    setPackageCategoryName(nextCategoryName)
+    setTargetItems(getTargetItems(nextCategoryName))
+  }
+
+  const handleChangeTargetName: ChangeEventHandler<HTMLSelectElement> = (event) => {
+    const { currentTarget: { value } } = event
+    const parseTarget = {
+      ...packageName,
+      target: value,
+    }
+    const result = safeParse(packageNameSchema, parseTarget)
+    const newData = result.success ? result.output : DEFAULT_PACKAGE_NAME
+    setPackageTargetName(newData.target)
+    if (newData.target === DEFAULT_PACKAGE_NAME.target) return
+    router.push(`/eslint-${newData.category}-${newData.target}`)
+  }
+
+  const handleChangePackageName: ChangeEventHandler<HTMLSelectElement> = (event) => {
+    const { currentTarget: { value } } = event
+    const [linter, category, target] = value.split("-")
+    const parseTarget = { category, linter, target }
+    const result = safeParse(packageNameSchema, parseTarget)
+    const newData = result.success ? result.output : DEFAULT_PACKAGE_NAME
+    setPackageName(newData)
+    router.push(`/${newData.linter}-${newData.category}-${newData.target}`)
+  }
+
+  return { handleChangeCategoryName, handleChangePackageName, handleChangeTargetName }
+}
