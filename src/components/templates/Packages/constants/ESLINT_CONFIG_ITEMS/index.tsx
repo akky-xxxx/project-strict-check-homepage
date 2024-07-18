36cbe6df4835a229fd73ff9196b3da0116a2065d
akky-xxxx <114.s.aki@gmail.com>
2024-07-19 00:23:35 +0900

    feat: define ESLINT_CONFIG_ITEMS

---
+++ src/components/templates/Packages/constants/ESLINT_CONFIG_ITEMS/index.tsx
@@ -0,0 +1,45 @@
+import { EslintConfigAll } from "../../components/atoms/EslintConfigAll"
+import { EslintConfigJest } from "../../components/atoms/EslintConfigJest"
+import { EslintConfigJs } from "../../components/atoms/EslintConfigJs"
+import { EslintConfigNext } from "../../components/atoms/EslintConfigNext"
+import { EslintConfigReact } from "../../components/atoms/EslintConfigReact"
+import { EslintConfigStorybook } from "../../components/atoms/EslintConfigStorybook"
+import { EslintConfigTs } from "../../components/atoms/EslintConfigTs"
+
+export const ESLINT_CONFIG_ITEMS = [
+  {
+    title: "eslint-config-sc-all",
+
+    contents: <EslintConfigAll />,
+  },
+  {
+    title: "eslint-config-sc-js",
+
+    contents: <EslintConfigJs />,
+  },
+  {
+    title: "eslint-config-sc-ts",
+
+    contents: <EslintConfigTs />,
+  },
+  {
+    title: "eslint-config-sc-jest",
+
+    contents: <EslintConfigJest />,
+  },
+  {
+    title: "eslint-config-sc-react",
+
+    contents: <EslintConfigReact />,
+  },
+  {
+    title: "eslint-config-sc-next",
+
+    contents: <EslintConfigNext />,
+  },
+  {
+    title: "eslint-config-sc-storybook",
+
+    contents: <EslintConfigStorybook />,
+  },
+]
