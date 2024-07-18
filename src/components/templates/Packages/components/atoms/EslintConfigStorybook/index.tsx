664e744105108a57092eea330da66413238bf52f
akky-xxxx <114.s.aki@gmail.com>
2024-07-19 00:23:00 +0900

    feat: implement EslintConfigStorybook

---
+++ src/components/templates/Packages/components/atoms/EslintConfigStorybook/index.tsx
@@ -0,0 +1,17 @@
+import { Link } from "../../../../../atoms/Link"
+
+import type { FC } from "react"
+
+export const EslintConfigStorybook: FC = () => (
+  <p>
+    This package sets some rules based on the shared config of
+    {" "}
+    <Link
+      isExternal
+      href="https://www.npmjs.com/package/eslint-plugin-storybook"
+    >
+      eslint-plugin-storybook
+    </Link>
+    .
+  </p>
+)
