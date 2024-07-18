c487149b7af6a959fd64db98ee9ae426b05b26af
akky-xxxx <114.s.aki@gmail.com>
2024-07-19 00:23:45 +0900

    feat: define ESLINT_PLUGIN_ITEMS

---
+++ src/components/templates/Packages/constants/ESLINT_PLUGIN_ITEMS/index.tsx
@@ -0,0 +1,9 @@
+import { EslintPluginJavascript } from "../../components/atoms/EslintPluginJavascript"
+
+export const ESLINT_PLUGIN_ITEMS = [
+  {
+    title: "eslint-plugin-sc-js",
+
+    contents: <EslintPluginJavascript />,
+  },
+]
