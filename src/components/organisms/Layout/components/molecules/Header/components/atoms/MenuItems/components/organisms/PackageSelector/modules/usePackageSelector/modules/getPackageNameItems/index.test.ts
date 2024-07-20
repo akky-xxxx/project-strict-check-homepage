5ae827dbe62fffa996969cbe6813ebe5a89b2d0b
akky-xxxx <114.s.aki@gmail.com>
2024-07-20 17:28:38 +0900

    feat: add initial value to getPackageNameItem

--- src/components/organisms/Layout/components/molecules/Header/components/atoms/MenuItems/components/organisms/PackageSelector/modules/usePackageSelector/modules/getPackageNameItems/index.test.ts
+++ src/components/organisms/Layout/components/molecules/Header/components/atoms/MenuItems/components/organisms/PackageSelector/modules/usePackageSelector/modules/getPackageNameItems/index.test.ts
@@ -3,6 +3,7 @@ import { getPackageNameItems } from "."
 describe("getPackageNameItem", () => {
   it("戻り値は %s", () => {
     const output = [
+      "------",
       "eslint-config-sc-all",
       "eslint-config-sc-jest",
       "eslint-config-sc-js",
