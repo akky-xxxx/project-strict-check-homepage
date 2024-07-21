4ba1007fe1f02aa70c64072cb7044a52dcc3eaa7
akky-xxxx <114.s.aki@gmail.com>
2024-07-21 21:34:42 +0900

    feat: use EslintConfigSCTs at routing component

--- src/app/packages/eslint-config-sc-ts/page.tsx
+++ src/app/packages/eslint-config-sc-ts/page.tsx
@@ -1,5 +1,7 @@
+import { EslintConfigSCTs } from "../../../components/templates/EslintConfigSCTs"
+
 export const runtime = "edge"
 
-const Page = () => <div>/eslint-config-ts</div>
+const Page = () => <EslintConfigSCTs />
 
 export default Page
