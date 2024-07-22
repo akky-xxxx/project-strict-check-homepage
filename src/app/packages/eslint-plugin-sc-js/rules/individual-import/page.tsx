84d765d88930dfe7aa4cfa3cdecaa6120ddba971
akky-xxxx <114.s.aki@gmail.com>
2024-07-22 23:43:12 +0900

    feat: use EslintPluginSCJsRulesIndividualImport at routing component

---
+++ src/app/packages/eslint-plugin-sc-js/rules/individual-import/page.tsx
@@ -0,0 +1,7 @@
+import { EslintPluginSCJsRulesIndividualImport } from "../../../../../components/templates/EslintPluginSCJsRulesIndividualImport"
+
+export const runtime = "edge"
+
+const Page = () => <EslintPluginSCJsRulesIndividualImport />
+
+export default Page
