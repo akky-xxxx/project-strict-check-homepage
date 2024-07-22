64d4335a9391153ea290ba39285f7394e0ffc4e6
akky-xxxx <114.s.aki@gmail.com>
2024-07-22 22:59:22 +0900

    feat: use EslintPluginSCJsRulesFilePathPatterns at routing component

---
+++ src/app/packages/eslint-plugin-sc-js/rules/file-path-patterns/page.tsx
@@ -0,0 +1,7 @@
+import { EslintPluginSCJsRulesFilePathPatterns } from "../../../../../components/templates/EslintPluginSCJsRulesFilePathPatterns"
+
+export const runtime = "edge"
+
+const Page = () => <EslintPluginSCJsRulesFilePathPatterns />
+
+export default Page
