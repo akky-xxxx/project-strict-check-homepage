142ac5b77864ba04f4adb80d051795a3cfaf3bea
akky-xxxx <114.s.aki@gmail.com>
2024-07-22 23:16:44 +0900

    feat: use EslintPluginSCJsRulesForbiddenMultipleNamedExports at routing component

---
+++ src/app/packages/eslint-plugin-sc-js/rules/forbidden-multiple-named-exports/page.tsx
@@ -0,0 +1,7 @@
+import { EslintPluginSCJsRulesForbiddenMultipleNamedExports } from "../../../../../components/templates/EslintPluginSCJsRulesForbiddenMultipleNamedExports"
+
+export const runtime = "edge"
+
+const Page = () => <EslintPluginSCJsRulesForbiddenMultipleNamedExports />
+
+export default Page
