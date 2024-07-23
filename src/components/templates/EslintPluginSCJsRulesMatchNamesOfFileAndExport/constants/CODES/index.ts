72a14dd6417da409ba4335cb22d5667b6c069892
akky-xxxx <114.s.aki@gmail.com>
2024-07-23 22:22:08 +0900

    feat: fix typo

--- src/components/templates/EslintPluginSCJsRulesMatchNamesOfFileAndExport/constants/CODES/index.ts
+++ src/components/templates/EslintPluginSCJsRulesMatchNamesOfFileAndExport/constants/CODES/index.ts
@@ -12,7 +12,7 @@ export const CODES = {
 // /components/atoms/Button/index.tsx
 export const button = () => {}`,
 
-  CORRECT_EXAMPLE: `// "sc-js/match-names-of-file-and-export": ["error", { "captures": [/\\/components\\/(?:atoms|molecules|organsism|templates)\\/([^/]+)\\/index.tsx/]] }]
+  CORRECT_EXAMPLE: `// "sc-js/match-names-of-file-and-export": ["error", { "captures": [/\\/components\\/(?:atoms|molecules|organisms|templates)\\/([^/]+)\\/index.tsx/]] }]
 // /components/atoms/Button/index.tsx
 export const Button = () => {}`,
 } as const
