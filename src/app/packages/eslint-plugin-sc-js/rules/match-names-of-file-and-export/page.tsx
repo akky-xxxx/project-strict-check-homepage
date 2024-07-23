7969630a8a0ff8c9edd78b7909669e0e8e7c33ac
akky--xxxx <114.s.aki@gmail.com>
2024-07-23 10:34:24 +0900

    feat: use EslintPluginSCJsRulesMatchNamesOfFileAndExport at routing component

---
+++ src/app/packages/eslint-plugin-sc-js/rules/match-names-of-file-and-export/page.tsx
@@ -0,0 +1,7 @@
+import { EslintPluginSCJsRulesMatchNamesOfFileAndExport } from "../../../../../components/templates/EslintPluginSCJsRulesMatchNamesOfFileAndExport"
+
+export const runtime = "edge"
+
+const Page = () => <EslintPluginSCJsRulesMatchNamesOfFileAndExport />
+
+export default Page
