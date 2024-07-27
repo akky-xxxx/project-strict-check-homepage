93cae22058d03ec5f9cc22c4ee179b5bcbec01bb
akky-xxxx <114.s.aki@gmail.com>
2024-07-27 18:16:34 +0900

    feat: change button to anchor

--- src/components/atoms/HeadingHash/index.tsx
+++ src/components/atoms/HeadingHash/index.tsx
@@ -1,7 +1,5 @@
 "use client"
 
-import toast from "react-hot-toast"
-
 import { css } from "@panda/css"
 
 import type { FC } from "react"
@@ -15,21 +13,7 @@ export const HeadingHash: FC<Props> = (props) => {
 
   if (hash == null) return null
 
-  const onClick = async () => {
-    const url = new URL(window.location.href)
-    const hashedUrl = `${url.origin}${url.pathname}#${hash}`
-    await navigator.clipboard.writeText(hashedUrl)
-    toast.success(`Copied the link to #${hash}.`, {
-      style: {
-        backgroundColor: "var(--colors-background-color-case2)",
-        border: "var(--colors-color-accent) 1px solid",
-        color: "var(--colors-color)",
-      },
-    })
-  }
-
-  // eslint-disable-next-line @typescript-eslint/no-misused-promises
-  return <button className={root} type="button" onClick={onClick}>#</button>
+  return <a className={root} href={`#${hash}`}>#</a>
 }
 
 const root = css({
