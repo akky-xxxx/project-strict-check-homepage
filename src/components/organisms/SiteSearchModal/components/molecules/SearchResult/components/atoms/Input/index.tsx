307993d9f7550e65ff091b7802fc128b373d64c8
akky-xxxx <114.s.aki@gmail.com>
2024-08-02 23:30:18 +0900

    feat: implement Input

---
+++ src/components/organisms/SiteSearchModal/components/molecules/SearchResult/components/atoms/Input/index.tsx
@@ -0,0 +1,70 @@
+import { cx } from "@panda/css"
+
+import { ResetIcon } from "./components/atoms/ResetIcon"
+import { SearchIcon } from "./components/atoms/SearchIcon"
+import { styles } from "./styles"
+
+import type { ChangeEventHandler, FC, MutableRefObject } from "react"
+
+type Props = {
+  enableReset: boolean
+  handleChangeSearchWord: ChangeEventHandler<HTMLInputElement>
+  handleResetSearchWord: () => void
+  inputRef: MutableRefObject<HTMLInputElement | null>
+  searchWord: string
+}
+
+const {
+  disableResetButton,
+  iconWrapper,
+  input,
+  label,
+  resetButton,
+  resetIconWrapper,
+  root,
+  searchIconWrapper,
+} = styles
+
+export const Input: FC<Props> = (props) => {
+  const {
+    enableReset,
+    handleChangeSearchWord,
+    handleResetSearchWord,
+    inputRef,
+    searchWord,
+  } = props
+
+  const resetButtonStyles = [
+    resetButton,
+    !enableReset && disableResetButton,
+  ]
+
+  return (
+    <div className={root}>
+      <label aria-label="Input search word" className={label}>
+        <div className={cx(iconWrapper, searchIconWrapper)}>
+          <SearchIcon />
+        </div>
+        <input
+          ref={inputRef}
+          className={input}
+          placeholder="Search"
+          type="text"
+          value={searchWord}
+          onChange={handleChangeSearchWord}
+        />
+      </label>
+      <div className={cx(iconWrapper, resetIconWrapper)}>
+        <button
+          aria-label="Reset search word"
+          className={cx(...resetButtonStyles)}
+          disabled={!enableReset}
+          type="button"
+          onClick={handleResetSearchWord}
+        >
+          <ResetIcon />
+        </button>
+      </div>
+    </div>
+  )
+}
