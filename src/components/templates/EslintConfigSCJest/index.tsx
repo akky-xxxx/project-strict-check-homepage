579dbe4b0ec4e2dfaaa5853d64a0f94563dac098
akky-xxxx <114.s.aki@gmail.com>
2024-08-03 17:30:20 +0900

    refactor: replace id, heading text to HEADINGS_INFO of eslint-config-sc-jest

--- src/components/templates/EslintConfigSCJest/index.tsx
+++ src/components/templates/EslintConfigSCJest/index.tsx
@@ -1,3 +1,5 @@
+import { HEADINGS_INFO } from "@shared/constants/HEADINGS_INFO"
+
 import { Installation } from "./components/atoms/Installation"
 import { Setup } from "./components/atoms/Setup"
 import { EslintConfigJestIntroduction } from "../../atoms/EslintConfigJestIntroduction"
@@ -7,13 +9,22 @@ import { Section } from "../../atoms/Section"
 
 import type { FC } from "react"
 
+const {
+  ESLINT_CONFIG_JEST: {
+    HOME: {
+      PAGE_TITLE,
+      USAGE,
+    },
+  },
+} = HEADINGS_INFO
+
 export const EslintConfigSCJest: FC = () => (
   <div>
-    <Heading1 id="package-name">eslint-config-sc-jest</Heading1>
+    <Heading1 id={PAGE_TITLE.hash}>{PAGE_TITLE.name}</Heading1>
     <EslintConfigJestIntroduction />
 
     <Section>
-      <Heading2 id="usage">Usage</Heading2>
+      <Heading2 id={USAGE.hash}>{USAGE.name}</Heading2>
       <Section>
         <Installation />
       </Section>
