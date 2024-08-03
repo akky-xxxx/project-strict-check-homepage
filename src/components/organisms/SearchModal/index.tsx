ce5960ca490915f2261f382ed36a7f48f18f5b14
akky-xxxx <114.s.aki@gmail.com>
2024-08-03 15:28:52 +0900

    feat: implement SearchModal

---
+++ src/components/organisms/SearchModal/index.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+import { useSearchModal } from "@shared/utils/useSearchModal"
+
+import { SearchModalBase } from "./components/organisms/SearchModalBase"
+import { Portal } from "../../atoms/Portal"
+
+import type { FC } from "react"
+
+export const SearchModal: FC = () => {
+  const { isShown, handleCloseModal } = useSearchModal((state) => state)
+
+  return (
+    <Portal>
+      <SearchModalBase handleCloseModal={handleCloseModal} isShown={isShown} />
+    </Portal>
+  )
+}
