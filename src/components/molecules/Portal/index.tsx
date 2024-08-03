67d02ba0b32049507eaee8912c7b098dfa45b883
akky-xxxx <114.s.aki@gmail.com>
2024-08-03 16:07:48 +0900

    feat: check to the mounted

--- src/components/molecules/Portal/index.tsx
+++ src/components/molecules/Portal/index.tsx
@@ -1,8 +1,17 @@
+import { useEffect, useState } from "react"
 import { createPortal } from "react-dom"
 
 import type { FCWithChildren } from "@shared/types/FCWithChildren"
 
 export const Portal: FCWithChildren = (props) => {
   const { children } = props
+  const [isMounted, setIsMounted] = useState(false)
+
+  useEffect(() => {
+    setIsMounted(true)
+  }, [])
+
+  if (!isMounted) return null
+
   return createPortal(children, document.body)
 }
