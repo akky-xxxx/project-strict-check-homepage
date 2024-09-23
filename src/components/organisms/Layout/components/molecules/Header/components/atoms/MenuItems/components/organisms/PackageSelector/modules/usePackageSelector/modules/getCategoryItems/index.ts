26a6282fa0892b81a984bde6201d0dd72718dfea
akky-xxxx <114.s.aki@gmail.com>
2024-09-23 14:59:55 +0900

    refactor: resolve sonarjs/no-alphabetical-sort error

--- src/components/organisms/Layout/components/molecules/Header/components/atoms/MenuItems/components/organisms/PackageSelector/modules/usePackageSelector/modules/getCategoryItems/index.ts
+++ src/components/organisms/Layout/components/molecules/Header/components/atoms/MenuItems/components/organisms/PackageSelector/modules/usePackageSelector/modules/getCategoryItems/index.ts
@@ -3,6 +3,6 @@ import { PACKAGE_NAMES } from "../../constants/PACKAGE_NAMES"
 import type { PackageName } from "@shared/types/PackageName"
 
 export const getCategoryItems = () => Object.keys(PACKAGE_NAMES.eslint)
-  .sort()
+  .sort((a, b) => a.localeCompare(b))
   // @ts-expect-error PACKAGE_NAMES を schema で縛ってるため type guard しないで握りつぶす
   .map<PackageName["category"]>((categoryName) => (categoryName))
